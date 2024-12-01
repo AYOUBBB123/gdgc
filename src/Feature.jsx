2239b0d7547b0e4f1efc8813d7f0deb0e8c3435f
Ayoubbezai2024 <158614538+AYOUBBB123@users.noreply.github.com>
2024-12-01 23:31:04 +0100

    Update Feature.jsx

--- src/Feature.jsx
+++ src/Feature.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const Feature = ({bg,name , img ,desc}) => {
   return (
 
-      <div className='flex flex-wrap lg:flex-nowrap pb-3 space-x-3'>
-        <img  className={` rounded-xl py-2 px-4 bg-${bg}  ` } src={img} alt="" />
+      <div className='w-1/6 flex flex-wrap lg:flex-nowrap pb-3 space-x-3'>
+        <img  className={` rounded-xl py-2 px- bg-${bg}  ` } src={img} alt="" />
         <div className='w-full'>
         <p className='font-semibold text-[14px]'>{name}</p>
         <span className='w-full text-sm text-gray-800'>{desc}</span>
@@ -17,4 +17,5 @@ const Feature = ({bg,name , img ,desc}) => {
 }
 
 export default Feature
+
 
